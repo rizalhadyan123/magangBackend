66266304fcd8bdf596de47bbd3ee40b277b6717a
rizalhadyan123 <rhfadhlillah@gmail.com>
2021-04-16 17:01:26 +0700

    change Port

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ fastify.get("/", async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    // await fastify.listen(process.env.PORT, '0.0.0.0');
-    await fastify.listen(3000);
+    await fastify.listen(process.env.PORT, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
