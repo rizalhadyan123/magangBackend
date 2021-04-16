548e6f19a2770c3fd7f6df2a912f5699591b8bcd
rizalhadyan123 <rhfadhlillah@gmail.com>
2021-04-16 16:44:48 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Require the framework and instantiate it
+const fastify = require("fastify")({ logger: true });
+const path = require("path");
+// Register plugins
+
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "/public"),
+  prefix: "/", //
+});
+
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "/public/assets"),
+  prefix: "/assets", //
+  decorateReply: false,
+});
+
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "/public/forms"),
+  prefix: "/forms",
+  decorateReply: false,
+});
+
+// Declare a route
+fastify.get("/", async (request, reply) => {
+  reply.sendFile("index.html"); // serving path.join(__dirname, 'public', 'index.html') directly
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    // await fastify.listen(process.env.PORT, '0.0.0.0');
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
